3fc1961d872b5ff78a3748132b77b8af48f104a2
Crispy <christophe-parmentier@laposte.net>
2018-01-08 15:28:54 +0100

    add slick slider for project section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,27 @@
 $(function() {
-		$('a[href*="#"]:not([href="#"])').on('click', function(e) {
-			e.preventDefault();
-			$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-		});
+ $('a[href*="#"]:not([href="#"])').on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({
+   scrollTop: $($(this).attr('href')).offset().top
+  }, 500, 'linear');
+ });
+
+
+});
+
+
+$(document).ready(function() {
+
+ $('.all-slides').slick({
+  infinite: true,
+  nextArrow: '<i class="fa fa-arrow-right arrows"></i>',
+  prevArrow: '<i class="fa fa-arrow-left arrows"></i>',
+  arrows: true,
+  speed: 500,
+  centerMode: true,
+  centerPadding: '0px',
+  cssEase: 'ease',
+  slidesToShow: 1,
+  adaptiveHeight: true
+ });
 });
