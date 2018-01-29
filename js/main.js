64a85c20708439d518899a9dfd3f58de53850532
Thomas <tg.59290@gmail.com>
2018-01-29 13:49:37 +0100

    added basic modal for RD part

--- js/main.js
+++ js/main.js
@@ -5,9 +5,32 @@ $(function() {
 		});
 });
 
+//Function to make the main title progressivly appear on home page
 function addOpacity() {
 	var title = document.getElementsByTagName("h1");
 	title[0].style.opacity = "1";
 }
 
 window.onload = addOpacity;
+
+//Function to deploy the rd section with projetcs
+
+var RDbutton = document.getElementById("RDmodal");
+
+var modalStatus = false;
+
+function changeHeight(pixelHeight) {
+	var RDsection = document.getElementById("RDsection");
+	RDsection.style.height = pixelHeight + "px";
+}
+
+RDbutton.onclick = function() {
+	if(modalStatus) {
+		changeHeight(0);
+		modalStatus = false;
+	}
+	else {
+		changeHeight(400);
+		modalStatus = true;
+	}
+};
