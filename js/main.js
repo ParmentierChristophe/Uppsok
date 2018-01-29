1ef7dcf6268f96cce874602387048291ea86d291
Thomas <tg.59290@gmail.com>
2018-01-29 09:56:16 +0100

    added css transition on title

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ $(function() {
 			$('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
 		});
 });
+
+function addOpacity() {
+	var title = document.getElementsByTagName("h1");
+	title[0].style.opacity = "1";
+}
+
+window.onload = addOpacity;
